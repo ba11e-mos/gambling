3c9879cc0476c152522b28a54c8c6b61b653a8a0
ba11e-mos <eplenoa@hotmail.com>
2025-03-11 13:00:37 +0100

    La til main.cpp og README.md

---
+++ README.md
@@ -0,0 +1,3 @@
+TDT4102 - Prosjekt
+
+Her ligger kun c++ kodefiler, build og backend lages på din pc for å sikre at det ikke skjer noe fucky.
---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include "std_lib_facilities.h"
+
+int main() {
+    cout << "Let's go gambling!" << endl;
+
+
+    return 0;
+}
+
+//------------------------------------------------------------------------------
